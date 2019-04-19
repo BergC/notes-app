b220d6024f7f92aa2f6243d51f22cf230882e8ea
Chris <christophertberg@gmail.com>
2019-04-18 19:40:14 -0700

    Begain working on the add and remove functionality for the notes app.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Importing our modules.
 const chalk = require('chalk'),
     yargs = require('yargs'),
-    getNotes = require('./notes.js');
+    notes = require('./notes.js');
 
-// Customize yargs verion.
+// Customize yargs version.
 yargs.version('1.1.0');
 
 // Create add command.
@@ -23,8 +23,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 });
 
@@ -32,8 +31,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove an old note',
-    handler: function() {
-        console.log('Remove an old note.');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.title);
     }
 });
 
---
+++ notes.js
@@ -0,0 +1,67 @@
+const fs = require('fs'),
+    chalk = require('chalk');
+
+const getNotes = function() {
+    return 'Your notes...';
+}
+
+// Functionality to add a note.
+const addNote = function(title, body) {
+    const notes = loadNotes();
+
+    // Ensure a note with that title doesn't already exist.
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title;
+    });
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+    
+        saveNotes(notes);
+
+        console.log(chalk.bgGreen('New note added!'));
+    } else {
+        console.log(chalk.bgRed('Note title taken'));
+    }
+}
+
+// Functionality to remove a note based on a provided title.
+const removeNote = function(title) {
+    const notes = loadNotes();
+
+    const retainedNotes = notes.filter(function(note) {
+        return note.title !== title;
+    });
+
+    if (retainedNotes.length < notes.length) {
+        console.log(chalk.bgGreen('Note removed!'));
+        saveNotes(retainedNotes); // We only need to save the notes if something is different.
+    } else {
+        console.log(chalk.bgRed('No note found!'));
+    }
+}
+
+// Functionality to save a note.
+const saveNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch(e) {
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
